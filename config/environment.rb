a6a88d460f5f20f542ff51f6c15d5692f5714326
Ahmed Soliman <ahmed.soliman@programmer.net>
2023-04-29 10:02:37 -0500

    added and configures sendgrid for account verification

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Setup Sendgrid
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plan,
+  user_name: Rails.application.credentials.sendgrid[:username],
+  password: Rails.application.credentials.sendgrid[:password],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}
